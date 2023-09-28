238883feff6a22b85348c7e2ae3a01096655c099
abnadan <ipc.abn22@gmail.com>
2023-09-28 08:49:43 -0300

    adicionado botao e readequado estilo

--- calculadora_imc/lib/homepage.dart
+++ calculadora_imc/lib/homepage.dart
@@ -12,10 +12,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("CALCULADORA DE IMC"),
+        title: Text(
+          "CALCULADORA DE IMC",
+          style: TextStyle(fontSize: 20),
+        ),
         centerTitle: true,
       ),
-      body: Text("teste"),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text("PESO:",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 20,
+            ),
+            Text("ALTURA:",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("apertou");
+        },
+      ),
     );
   }
 }
