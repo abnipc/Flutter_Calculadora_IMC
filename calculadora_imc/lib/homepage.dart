2da651787d42dbad8c88a045f6562bc0afae6ef8
abnadan <ipc.abn22@gmail.com>
2023-09-28 11:25:20 -0300

    logica inteira funcionando perfeitamente, trabalhando no estilho

--- calculadora_imc/lib/homepage.dart
+++ calculadora_imc/lib/homepage.dart
@@ -8,35 +8,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String peso = "";
+  String altura = "";
+  String imc_string = "";
+  double IMC = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "CALCULADORA DE IMC",
           style: TextStyle(fontSize: 20),
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Text("PESO:",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(
-              height: 20,
-            ),
-            Text("ALTURA:",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-          ],
-        ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const Text("PESO:",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    //labelText: 'teste',
+                    hintText: "dica de texto",
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  onChanged: (Text) {
+                    peso = Text;
+                  },
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              const Text("ALTURA:",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    //labelText: 'teste',
+                    hintText: "dica de texto",
+                    prefixIcon: Icon(Icons.height),
+                  ),
+                  onChanged: (Text) {
+                    altura = Text;
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "SEU IMC ATUAL É:",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            imc_string,
+            style: TextStyle(fontSize: 30),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.percent),
         onPressed: () {
-          print("apertou");
+          IMC = double.parse(peso) /
+              (double.parse(altura) * double.parse(altura));
+          imc_string = IMC.toString();
+
+          setState(() {
+            //set state é empregado para que o valor seja atualizado no campo correspondente
+            imc_string = IMC.toString();
+          });
         },
       ),
     );
