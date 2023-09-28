2c314f6d27791449a0a130dda308b6a4e3fa5b89
abnadan <ipc.abn22@gmail.com>
2023-09-28 11:52:28 -0300

    finalizado o exercicio para entrega a DIO

--- calculadora_imc/lib/homepage.dart
+++ calculadora_imc/lib/homepage.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String peso = "";
   String altura = "";
-  String imc_string = "";
+  String imc_string = "0";
   double IMC = 0;
 
   @override
@@ -23,62 +23,74 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              const Text("PESO:",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              Expanded(
-                child: TextField(
-                  decoration: const InputDecoration(
-                    //labelText: 'teste',
-                    hintText: "dica de texto",
-                    prefixIcon: Icon(Icons.person),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset(
+              'assets/minha_imagem.png',
+              width: 150,
+              height: 150,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text("PESO:",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      //labelText: 'teste',
+                      hintText: "Peso em kg",
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    onChanged: (Text) {
+                      peso = Text;
+                    },
                   ),
-                  onChanged: (Text) {
-                    peso = Text;
-                  },
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              const Text("ALTURA:",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              Expanded(
-                child: TextField(
-                  decoration: const InputDecoration(
-                    //labelText: 'teste',
-                    hintText: "dica de texto",
-                    prefixIcon: Icon(Icons.height),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                const Text("ALTURA:",
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      //labelText: 'teste',
+                      hintText: "Peso em metros, ex: 1.78",
+                      prefixIcon: Icon(Icons.height),
+                    ),
+                    onChanged: (Text) {
+                      altura = Text;
+                    },
                   ),
-                  onChanged: (Text) {
-                    altura = Text;
-                  },
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "SEU IMC ATUAL É:",
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            imc_string,
-            style: TextStyle(fontSize: 30),
-          ),
-        ],
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "SEU IMC ATUAL É:",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              imc_string,
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.percent),
