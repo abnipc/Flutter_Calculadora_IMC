7e0dd8b32d526492a8c3b18a8f161f1bd24e66f9
abnadan <ipc.abn22@gmail.com>
2023-09-28 08:26:52 -0300

    reorganizei as paginas e iniciei o uso consciente de scaffold

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -1,45 +1,20 @@
+import 'package:calculadora_imc/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      const MyApp()); // A classe MyApp é definida abaixo, aqui ela está sendo instanciada
+    MyApp(),
+  ); // A classe MyApp é definida abaixo, aqui ela está sendo instanciada
 }
 
-//classe MyApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(), //classe HomePage sendo instanciada
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
-  }
-}
-
-//definição da classe HomePage
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    //Scafold é um template de app
-    return Scaffold(
-        //adiciona barra ao topo do corpo do app
-        appBar: AppBar(
-          title: Text("CALCULADORA DE IMC"),
-        ),
-        //adiciona corpo
-        body: Column(
-          children: [
-            Text("Peso"),
-            Text("Altura"),
-          ],
-        ));
   }
 }
